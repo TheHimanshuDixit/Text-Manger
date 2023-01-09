264d3aade07c93940df04837259b7b008cc44780
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-01-09 15:02:18 +0530

    props added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Users from './Users';
+import App from './App';
+// import Users from './Users';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Users/>
+    <App/>    
+    {/* or used like this*/}
+    {/* <App></App> */}
   </React.StrictMode>
 );
 
@@ -16,3 +18,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+// if you did not want to export then write it in a same File.
+// function App() {
+//   return (
+//     <>
+//     <Navbar />
+//     {/* <Navbar  title="Text Manager" abouttext="About Text Manager" />              passing props it is like an argument (object) */}
+//     </>
+//   );
+// }
