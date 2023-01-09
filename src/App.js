0ee93d925b51297abcd00e7b6d707252265a75b1
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-01-10 03:08:46 +0530

    favicon , dynamic title, alert has been set

--- src/App.js
+++ src/App.js
@@ -4,33 +4,53 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
 import React, {useState} from 'react';
+import Alert from './Components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light');            // whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg : message,
+      type : type
+    })
+    setTimeout(()=>{
+      setAlert(null)
+    },2000)
+  }
 
   const toggleMode = () =>{
     if(mode === 'light')
     {
       setMode('dark');
       document.body.style.backgroundColor = 'black'
+      showAlert("Dark mode has been enabled","success")
+      // changing title dynamically
+      document.title = 'Text Manager - Dark Mode'
     }
     else
     {
       setMode('light');
       document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enabled","success")
+      document.title = 'Text Manager - Light Mode'
     }
   }
 
   return (
+    <>
+    <Navbar title="Text Manager" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className='App'>
       {/* <Navbar /> */}
       {/* passing props it is like an argument (object) */}
       {/* <Navbar  title="Text Manager" abouttext="About Text Manager" /> */}
-      <Navbar title="Text Manager" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-        <Textform heading="... ENTER THE TEXT BELOW AND ANALYZE ..." mode={mode}/>
+        <Textform heading="... ENTER THE TEXT BELOW AND ANALYZE ..." mode={mode} showAlert={showAlert}/>
       </div>
     </div>
+    </>
   );
 }
 export default App;
--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -7,62 +7,70 @@ export default function Textform(props) {
         // console.log("Uppercase was clicked: " +  text);
         let newText = text.toUpperCase();
         setText(newText)
+        props.showAlert("Converted to Uppercase","success")
     }
-
+    
     const handleLoClick = () => {
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert("Converted to Lowercase","success")
     }
-
+    
     const handleclearClick = () => {
         let newText = "";
         setText(newText)
+        props.showAlert("Textbox cleared","success")
     }
-
+    
     const handlecopyClick = () => {
+        props.showAlert("Text has been copy","success")
         navigator.clipboard.writeText(text)
     }
-
+    
     const handleRESClick = () => {
         let newText = text.replace(/[ ]+/g, ' ');;
         setText(newText)
+        props.showAlert("Extra space has been removed","success")
     }
-
+    
     const handlereverseClick = () => {
         let newText = text.split('').reverse().join('');
         setText(newText)
+        props.showAlert("Text Reversed","success")
     }
-
+    
     const handlecutClick = () => {
+        props.showAlert("Text has been cut","success")
         navigator.clipboard.writeText(text)
         let newText = "";
         setText(newText)
     }
-
+    
     const handleOnChange = (event) => {
         // console.log("On change");
         setText(event.target.value)
     }
-
+    
     const style = {
-        'font-weight': 'normal'
+        fontWeight: 'normal'
     }
     const [myStyle, setMyStyle] = useState(style);
     const [btn, setBtn] = useState("Bold Text");
-
+    
     const handleboldClick = () => {
-        if (myStyle['font-weight'] === "normal") {
+        if (myStyle.fontWeight === "normal") {
             setMyStyle({
-                'font-weight': 'bold'
+                fontWeight: 'bold'
             })
             setBtn("Normal Text")
         }
         else {
             setMyStyle({
-                'font-weight': 'normal'
+                fontWeight: 'normal'
             })
             setBtn("Bold Text")
         }
+        props.showAlert("Text converted","success")
     }
 
     function countword() {
@@ -86,7 +94,7 @@ export default function Textform(props) {
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
                     {/* <label for="myBox" className="form-label">Example textarea</label> */}
-                    <textarea style={{ 'font-weight': btn === 'Normal Text' ? 'bold' : 'normal', backgroundColor: props.mode === 'light' ? 'white' : '#212529', color: props.mode === 'light' ? 'black' : 'white' }} className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea style={{ fontWeight: btn === 'Normal Text' ? 'bold' : 'normal', backgroundColor: props.mode === 'light' ? 'white' : '#212529', color: props.mode === 'light' ? 'black' : 'white' }} className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
                 <button className={`btn btn-${props.mode === 'light' ? 'primary' : 'dark'} mx-1`} onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className={`btn btn-${props.mode === 'light' ? 'primary' : 'dark'} mx-1`} onClick={handleLoClick}>Convert to Lowercase</button>
@@ -105,7 +113,7 @@ export default function Textform(props) {
                 {/* <p>{0.008 * text.split(" ").length} Minutes read</p> */}
                 <p>{0.008 * countword()} Minutes to read</p>
                 <h2>-: Preview :-</h2>
-                <p style={myStyle}>{text.length > 0 ? text : "Enter Something in the textbox above to preview it here"}</p>
+                <p style={myStyle}>{text.length > 0 ? text : "Enter Something in the textbox above to preview it here..."}</p>
             </div>
         </>
     )
